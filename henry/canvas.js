7ea79f38851c4b8f49fae9f7a1e928d6f3bb3f84
BlazingFish <115965185+BlazingFish@users.noreply.github.com>
2025-09-10 10:00:24 +0930

    Update canvas.js

--- henry/canvas.js
+++ henry/canvas.js
@@ -2,19 +2,22 @@
 const canvas = document.getElementById("canvas"); //Drawing canvas ID
 const ctx = canvas.getContext("2d");
 
+var mouseDown;
+
 canvas.onclick = function(event) {
     if (event.which == 1) { //Detects if it is left click
+        mouseDown = true
         draw(event);
     };
 };
 
-canvas.webkitMatchesSelector(mouseDown) {
-    
-}
+// canvas.while(mouseDown) {
+//     console.log("s")
+// }
 
-function draw(event) {
+function draw() {
     ctx.fillStyle = "red";
-    ctx.fillRect(event.clientX, event.clientY, 1, 1);
+    ctx.fillRect(event.pageX, event.pageY, 1, 1);
     console.log(this.value=event.clientX+':'+event.clientY); //I just copied this from docs, I have no clue what 'this' or 'event' is.
 };
 
