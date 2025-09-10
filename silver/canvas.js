bd40bd62caa5633251342d7c83ba5b7e0795ee2c
BlazingFish <115965185+BlazingFish@users.noreply.github.com>
2025-09-10 09:40:35 +0930

    Update canvas.js

--- silver/canvas.js
+++ silver/canvas.js
@@ -1,5 +1,7 @@
+// HENRY CODE STARTS HERE
 const canvas = document.getElementById("canvas"); //Drawing canvas ID
 const ctx = canvas.getContext("2d");
 
 ctx.fillStyle = "red";
 ctx.fillRect(0, 0, 150, 75);
+// HENRY CODE ENDS HERE
