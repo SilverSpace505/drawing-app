37ab5fc6aaf5a7c586d0adf74808b3fe02b174a5
Silver <112796153+SilverSpace505@users.noreply.github.com>
2025-09-11 13:17:06 +0930

    upload and download

--- henry/network.js
+++ henry/network.js
@@ -1,3 +1,9 @@
+
+const { createClient } = supabase;
+const supabaseUrl = 'https://hfbnrnmfhierhtlhcute.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmYm5ybm1maGllcmh0bGhjdXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NjI3NTQsImV4cCI6MjA3MzAzODc1NH0.1MJcf4GxfhBP4qLzJuTvnh4iOr2ELjZ2YrXxKnO-AiM'
+const client = createClient(supabaseUrl, supabaseKey);
+
 async function createDrawing(name, data, description = null) {
   const { data2, error } = await client
   .from('drawings')
@@ -27,4 +33,29 @@ async function createAccount(email, password) {
     password,
   })
   console.log(data, error)
+}
+
+const uploadBtn = document.getElementById('uploadBtn')
+const uploadName = document.getElementById('uploadName')
+const uploadDescription = document.getElementById('uploadDescription')
+
+uploadBtn.onclick = async () => {
+  const {data, error} = await client.auth.getUser()
+  console.log(data, error)
+  return
+  createDrawing(uploadName.value, save(), uploadDescription.value)
+}
+
+const downloadBtn = document.getElementById('downloadBtn')
+const downloadName = document.getElementById('downloadName')
+
+downloadBtn.onclick = async() => {
+  const { data, error } = await client
+  .from('drawings')
+  .select("*")
+
+  // Filters
+  .eq('name', downloadName.value)
+  // console.log(data)
+  load(data[0].data)
 }
