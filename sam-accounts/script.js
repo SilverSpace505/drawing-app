cde2b0b5bcc8292b9c588e3641742cf7f6547434
redguyonetwo <151694493+Redguyonetwo@users.noreply.github.com>
2025-09-18 12:31:06 +0930

    half commented

--- sam-accounts/script.js
+++ sam-accounts/script.js
@@ -1,3 +1,5 @@
+//Get all HTML elements
+
 const email = document.getElementById('signUpEmail')
 const username = document.getElementById('username')
 const password = document.getElementById('signUpPassword')
@@ -8,14 +10,17 @@ const pw2 = document.getElementById('logInPassword')
 const signUpForm = document.getElementById('signUpContainer')
 const logInForm = document.getElementById('logInContainer')
 
+//Set up global variables
 let user = null;
 let profileData = null;
 
 function signUp() {
+    //Show the form container (CSS allows for smooth animation)
     signUpForm.style.top = '15%'
 }
 
 function logMeIn() {
+  //Show the form container (CSS allows for smooth animation)
     logInForm.style.top = '15%'
 }
 
@@ -23,27 +28,33 @@ function addAccount() {
     if (!email.value || 
         !password.value || 
         !confirmPW.value
+        //not needed since they all have 'required' but understandable 
     ) {
         console.log('didnt fill it in')
+        //don't do anything if not filled in
         return
     }
     console.log('adding')
     if (password.value != confirmPW.value) {
+        //they should be the same, to ensure no typos are present
         console.log('the password doesnt match')
         return
     }
 
     createAccount(email.value, password.value)
+    //hide the container
     signUpForm.style.top = '100%'
 }
 
 function loginHTML() {
+  //helper function for doing all the stuff a button onclick should do
     if (!email2.value || !pw2.value) {
         console.log('didnt fill it in')
         return
     }
 
     login(email2.value, pw2.value)
+    //hide the container
     logInForm.style.top = '100%'
 }
 
@@ -55,26 +66,18 @@ function closeLogIn() {
   logInForm.style.top = '100%'
 }
 
+//create database reference
 const { createClient } = supabase;
-const supabaseUrl = 'https://hfbnrnmfhierhtlhcute.supabase.co'
+const supabaseUrl = 'https://hfbnrnmfhierhtlhcute.supabase.co' //our unique request URL
+//our API key (one key, but can check if user.admin for specific commands)
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhmYm5ybm1maGllcmh0bGhjdXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NjI3NTQsImV4cCI6MjA3MzAzODc1NH0.1MJcf4GxfhBP4qLzJuTvnh4iOr2ELjZ2YrXxKnO-AiM'
 const client = createClient(supabaseUrl, supabaseKey);
 
-async function createDrawing() {
-  const { data, error } = await client
-  .from('drawings')
-  .insert([
-    { data: {testing: 123} },
-  ])
-  .select()
-  console.log(data, error)
-}
-
 async function logout() {
   const { error } = await client.auth.signOut()
+  //get rid of supabase user - no longer able to fetch anything
   console.log(error)
-  console.log('this should give an error', await client.auth.getUser())
-  accName.innerText = 'Not Signed In'
+  if (!error) accName.innerText = 'Not Signed In'
 }
 
 async function login(email, password) {
